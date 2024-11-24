cb4ffec61d989714be6adb075d502e6621a6f75f
Aryo <aryobd@gmail.com>
2024-11-24 14:30:11 +0700

    PARAMETER --> OFFSET & LIMIT

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -31,6 +31,11 @@ on a.group_code = b.group_code
 
 where a.group_code = @group_code --> MENGGUNAKAN PARAMETER
 and a.class_code = @class_code --> MENGGUNAKAN PARAMETER
+
+order by a.group_code, a.class_code --> ORDER
+
+--offset 20 limit 10 --> PAGING --> MULAI DARI BARIS 21 SEBANYAK 10 BARIS
+offset @offset limit @limit --> PAGING --> MULAI DARI BARIS @offset + 1 SEBANYAK @limit BARIS
 ";
 
             // DEFINE THE PARAMETERS
@@ -44,8 +49,18 @@ and a.class_code = @class_code --> MENGGUNAKAN PARAMETER
                 Value = "1A" // ASSIGN A VALUE TO THE @class_code PARAMETER
             };
 
+            NpgsqlParameter param3 = new NpgsqlParameter("@offset", NpgsqlTypes.NpgsqlDbType.Integer)
+            {
+                Value = 0 // SET YOUR OFFSET VALUE (STARTING ROW, E.G., ROW 1)
+            };
+
+            NpgsqlParameter param4 = new NpgsqlParameter("@limit", NpgsqlTypes.NpgsqlDbType.Integer)
+            {
+                Value = 10 // SET YOUR LIMIT VALUE (NUMBER OF ROWS PER PAGE)
+            };
+
             // EXECUTE THE QUERY WITH PARAMETERS AND MAP TO THE MODEL
-            var qry = dbContext.Database.SqlQuery<CommTrClass>(sqlQuery, param1, param2).AsQueryable(); // WITH PARAMETERS
+            var qry = dbContext.Database.SqlQuery<CommTrClass>(sqlQuery, param1, param2, param3, param4).AsQueryable(); // WITH PARAMETERS
 
             // EXECUTE THE QUERY AND MAP TO THE MODEL
             //var qry = dbContext.Database.SqlQuery<CommTrClass>(sqlQuery).AsQueryable(); // WITHOUT PARAMETERS
